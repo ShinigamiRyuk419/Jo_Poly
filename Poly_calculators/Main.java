5e91bcf417f7cd3046c2c6122ddadcfa58498266
ShinigamiRyuk419 <112138312+ShinigamiRyuk419@users.noreply.github.com>
2022-11-19 11:29:19 +0800

    Updated this file

--- Poly_calculators/Main.java
+++ Poly_calculators/Main.java
@@ -1,10 +1,8 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Poly_calculators;
-
-import java.io.IOException;
 
 /**
  *
@@ -12,29 +10,74 @@ import java.io.IOException;
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) throws IOException {
-       Polynomial p = new Polynomial(4);             //Line 1
-       Polynomial q = new Polynomial(8);             //Line 2
+	public static void main(String[] args) 
+	{
+		Polynomial poly1 = new Polynomial();
+		Polynomial poly2 = new Polynomial();
+		Polynomial result1 = new Polynomial();
+                Polynomial result2 = new Polynomial();
 
-       p.read();                                     //Line 3
-       System.out.println();                         //Line 4
-       System.out.println("Line 5: p(x): " + p);     //Line 5
+		
+		// Add few terms to the first polynomial 
 
-       System.out.println("Line 6: p.evaluate(2): "
-                         + p.evaluate(2));           //Line 6
-       System.out.println();                         //Line 7
+                //(coefficient, exonent)
+                
+		poly1.add(1,5);		
+		poly1.add(-2,2);	
+		poly1.add(1,4);		
+		poly1.add(5,0);		
+		
+//                poly1.add(0,0); //to test if no polynomial defined
+		
+			
+		//Reduced to: 2x^3 - x^2 + 3 + 4x^4
+		poly2.add(2,3);		
+		poly2.add(-1,2);	
+		poly2.add(5,0);		
+		poly2.add(4,4);     	
+		poly2.add(-2,0);    	
+		poly2.add(0,0);     	//Discarded
+		poly2.add(0,3);     	//Discarded
+		
+		//to store the result of the addition function
+		result1 = Polynomial.addition(poly1, poly2);
+                result2 = Polynomial.subtraction(poly1, poly2);
+                
+                //to store the result of the subtraction function
+                
+                System.out.println("---------------------------------------------------------");
+		System.out.println("                Polynomial Calculator");
+                System.out.println("-------------------------------------------------------\n");
 
-       q.read();                                     //Line 8
-       System.out.println("Line 9: q(x): " + q);     //Line 9
 
-       System.out.println("Line 10: p(x) + q(x): "
-                         + p.add(q));                //Line 10
+		
+		if(poly1.size() > 0)
+                    Polynomial.printOut("1st Polynomial:\t\t\t\t", poly1);
+                else 
+                    System.out.println("No polynomial defined\n");
+                
+                
+		if(poly2.size() > 0)
+                    Polynomial.printOut("2nd Polynomial:\t\t\t\t", poly2);
+                else 
+                    System.out.println("No polynomial defined\n");
+		
+		if(poly1.size() > 1)
+                    Polynomial.printOut("1st Polynomial in descending order:\t", Polynomial.sort(poly1));
+                
+		if(poly2.size() > 1)
+                    Polynomial.printOut("2nd Polynomial in descending order:\t", Polynomial.sort(poly2));
+		
+		if(!(poly1.size() == 0 || poly2.size() == 0))
+                    Polynomial.printOut("Addition result:\n\n\t", Polynomial.sort(result1));
+                else 
+                    System.out.println("No addition operation performed\n");
+                
+                if(!(poly1.size() == 0 || poly2.size() == 0))
+                    Polynomial.printOut("Subtraction result:\n\n\t", Polynomial.sort(result2));
+                else 
+                    System.out.println("No addition operation performed\n");
 
-       System.out.println("Line 11: p(x) - q(x): "
-                         + p.subtract(q));           //Line 11
-    }
-    
+	}
 }
+
